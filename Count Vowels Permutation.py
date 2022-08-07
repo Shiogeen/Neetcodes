b58fa9219e03acc40cc503b9cace68657b7ca9af
Shiogeen <82872879+Shiogeen@users.noreply.github.com>
2022-08-07 22:21:45 +0800

    Create Count Vowels Permutation.py
    https://leetcode.com/problems/count-vowels-permutation/

---
+++ Count Vowels Permutation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        count = [1,1,1,1,1]
+        while True:
+            if n == 1:
+                return sum(count) % (10 ** 9 + 7)
+            count = [count[1] + count[2] + count[4], count[0] + count[2], count[1] + count[3], count[2], count[2] + count[3]]
+            n -= 1
+            
